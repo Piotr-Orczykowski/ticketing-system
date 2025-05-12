1966e93a9b4a5423b006ada56938e4bd6a11a291
Piotr Orczykowski <piotr@piterdev.com>
2025-05-12 23:32:41 +0100

    Add comment controller and routes for managing comments on tickets

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,18 @@
+// File: backend/controllers/authController.js
+// This code defines the controller functions for user authentication and profile management.
+
+exports.addComment = async (req, res) => {
+    res.send("Add Comment");
+};
+
+exports.getComments = async (req, res) => {
+    res.send("Get Comments");
+}
+
+exports.updateComment = async (req, res) => {
+    res.send("Update Comment");
+};
+
+exports.deleteComment = async (req, res) => {
+    res.send("Delete Comment");
+}
---
+++ backend/routes/commentRoutes.js
@@ -0,0 +1,16 @@
+// This code defines the routes for managing comments on tickets.  
+// It includes routes for adding a comment, getting comments for a ticket, updating a comment, and deleting a comment.
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const Ticket = require('../models/Ticket');
+const Comment = require('../models/Comment');
+const { addComment, getComments, updateComment, deleteComment } = require('../controllers/commentController');
+
+router.post('/add', addComment);
+router.get('/get/:ticketId', getComments);
+router.put('/update/:commentId', updateComment);
+router.delete('/delete/:commentId', deleteComment);
+
+exports = router;
