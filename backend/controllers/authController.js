df826f5130983f0c94980acac37c582d57e36c5c
Piotr Orczykowski <piotr@piterdev.com>
2025-05-13 18:11:34 +0100

    Eliminate double-hashing in model and controller for users

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,15 +12,14 @@ exports.registerUser = async (req, res) => {
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
-        const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({
             username,
             email,
-            password: hashedPassword,
+            password,
             role
         });
         await user.save();
-        res.status(201).json({ message: 'User registered successfully '});
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
