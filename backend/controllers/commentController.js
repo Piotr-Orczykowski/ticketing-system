90fb2a768f35ea967d449555c0d41ff7f08433d6
Piotr Orczykowski <piotr@piterdev.com>
2025-05-14 14:27:21 +0100

    Implement logic for adding a comment

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,8 +1,32 @@
 // File: backend/controllers/authController.js
 // This code defines the controller functions for user authentication and profile management.
+const Ticket = require('../models/Ticket');
+const Comment = require('../models/Comment');
 
 exports.addComment = async (req, res) => {
-    res.send("Add Comment");
+    try {
+        const { text } = req.body;
+        const ticketId = req.params.ticketIdl; // Assuming ticketId is passed as a URL parameter
+        const userId = req.user.id;
+
+        const ticket =  await Ticket.findById(ticketId);
+        if (!ticket) {
+            return res.status(404).json({ message: 'Ticket not found' });
+        }
+
+        const newComment = new Comment({
+            ticketId,
+            userId,
+            text
+        });
+        await newComment.save();
+        res.status(201).json({
+            message: 'Comment added successfully',
+            comment: newComment
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 exports.getComments = async (req, res) => {
